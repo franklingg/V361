2b471231828a00a4abe4c566ebb1b5f7eec78105
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2022-03-17 12:52:25 -0300

    feat: adiciona página inicial do sistema

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,14 @@
 
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/Home';
+import Board from './pages/Board';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Home} />
+        <Route path="/" element={<Home />} />
+        <Route path="/board" element={<Board />} />
         <Route path="*" element={<Navigate to="/" /> } />
       </Routes>
     </BrowserRouter>
---
+++ Frontend/src/pages/Board/Board.js
@@ -0,0 +1,7 @@
+import styles from './Board.module.css';
+
+export default function Board(){
+    return(
+        <h1>Hello world!</h1>
+    );
+}
