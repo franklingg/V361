9ef839fbcb294877b7fca5728fb5cc9dbdc6c31a
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2022-03-17 01:23:17 -0300

    doc: adiciona link para documentação da API no postman

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -4,7 +4,7 @@ router.get('/', (req, res) => {
     const template = `
         <div>
             <h1>API-V361</h1>
-            <a href="/">Acesse a documentação da API</a>
+            <a href="https://documenter.getpostman.com/view/16458498/UVsMukPB">Acesse a documentação da API</a>
         </div>
     `
     res.status(200).send(template);
